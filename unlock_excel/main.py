b129fa2ed56acd7ce0e2da3954f6802db7f06072
P3rdigas <pr.perdigao@campus.fct.unl.pt>
2023-12-20 23:16:30 +0000

    Improve change appearance

--- unlock_excel/main.py
+++ unlock_excel/main.py
@@ -59,9 +59,9 @@ class CrackExcel(customtkinter.CTk):
 
         self.settings_button_dropdown = CustomDropdownMenu(widget=self.settings_button, corner_radius=0, bg_color=(self.DROPDOWN_BACKGROUND_COLOR_LIGHT, self.DROPDOWN_BACKGROUND_COLOR_DARK), text_color=(self.TEXT_COLOR_LIGHT, self.TEXT_COLOR_DARK), hover_color=(self.HOVER_COLOR_LIGHT, self.HOVER_COLOR_DARK))
         appearance_sub_menu = self.settings_button_dropdown.add_submenu("Appearance")
-        appearance_sub_menu.add_option(option="Light", command=self.load_light_mode)
-        appearance_sub_menu.add_option(option="Dark", command=self.load_dark_mode)
-        appearance_sub_menu.add_option(option="System", command=self.load_system_mode)
+        appearance_sub_menu.add_option(option="Light", command=lambda:self.change_appearance_mode_event("light"))
+        appearance_sub_menu.add_option(option="Dark", command=lambda:self.change_appearance_mode_event("dark"))
+        appearance_sub_menu.add_option(option="System", command=lambda:self.change_appearance_mode_event("system"))
 
         self.about_button_dropdown = CustomDropdownMenu(widget=self.about_button, corner_radius=0, bg_color=(self.DROPDOWN_BACKGROUND_COLOR_LIGHT, self.DROPDOWN_BACKGROUND_COLOR_DARK), text_color=(self.TEXT_COLOR_LIGHT, self.TEXT_COLOR_DARK), hover_color=(self.HOVER_COLOR_LIGHT, self.HOVER_COLOR_DARK))
         self.about_button_dropdown.add_option(option="Source Code", image=self.github_image, command=self.open_browser)
@@ -117,149 +117,29 @@ class CrackExcel(customtkinter.CTk):
             theme = self.config.get('Settings', 'theme')
 
             if theme == "system":
-                self.load_init_system_mode()
+                customtkinter.set_appearance_mode("system")
             elif theme == "light":
                 customtkinter.set_appearance_mode("light")
-                self.load_init_light_mode()
             else:
                 customtkinter.set_appearance_mode("dark")
-                self.load_init_dark_mode()
         else:
             self.config['Settings'] = {'theme': 'system'}
             with open(self.config_file, 'w') as configfile:
                 self.config.write(configfile)
-            
-            self.load_init_system_mode()
-    
-    # def load_init_light_mode(self):
-    #     # self.menu_bar_bg = self.MENUBAR_BACKGROUND_COLOR_LIGHT
-    #     # self.menu_bar_text_color = self.TEXT_COLOR_LIGHT
-    #     # self.menu_bar_hover_color = self.HOVER_COLOR_LIGHT
-    #     self.dropdown_bg_color = self.DROPDOWN_BACKGROUND_COLOR_LIGHT
-    #     self.dropdown_text_color = self.TEXT_COLOR_LIGHT
-    #     self.dropdown_hover_color = self.HOVER_COLOR_LIGHT
 
-    # def load_init_dark_mode(self):
-    #     # self.menu_bar_bg = self.MENUBAR_BACKGROUND_COLOR_DARK
-    #     # self.menu_bar_text_color = self.TEXT_COLOR_DARK
-    #     # self.menu_bar_hover_color = self.HOVER_COLOR_DARK
-    #     self.dropdown_bg_color = self.DROPDOWN_BACKGROUND_COLOR_DARK
-    #     self.dropdown_text_color = self.TEXT_COLOR_DARK
-    #     self.dropdown_hover_color = self.HOVER_COLOR_DARK
+    def change_appearance_mode_event(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
 
-    # def load_init_system_mode(self):
-    #     customtkinter.set_appearance_mode("system")
-    #     appearance_mode = customtkinter.get_appearance_mode()
+        self.config.set('Settings', 'theme', new_appearance_mode)
 
-    #     if appearance_mode == "light":
-    #         self.load_init_light_mode()
-    #     else:
-    #         self.load_init_dark_mode()
-
-    # def load_images(self, text):
-    #     match text:
-    #         case "Source Code":
-    #             return customtkinter.CTkImage(light_image=Image.open(self.GITHUB_LOGO_LIGHT_PATH), dark_image=Image.open(self.GITHUB_LOGO_DARK_PATH))
-
-    # def updated_dropdown(self, widget, original_dropdown, bg_color, text_color, hover_color):
-    #     new_dropdown = CustomDropdownMenu(
-    #         widget=widget,
-    #         corner_radius=0,
-    #         bg_color=bg_color,
-    #         text_color=text_color,
-    #         hover_color=hover_color
-    #     )
-
-    #     for option in original_dropdown._options_list:
-    #         if isinstance(option, dropdown_menu._CDMSubmenuButton):
-    #             submenu_copy = new_dropdown.add_submenu(option.cget("text"), bg_color=bg_color)
-    #             for sub_option in option.submenu._options_list:
-    #                 image = None
-
-    #                 if hasattr(sub_option, '_image') and sub_option._image is not None:
-    #                     image = self.load_images(option.cget("text"))
-                        
-    #                 submenu_copy.add_option(
-    #                     option=sub_option.cget("text"),
-    #                     image = image,
-    #                     command=sub_option._command
-    #                 )
-    #         else:
-    #             image = None
-                
-    #             if hasattr(option, '_image') and option._image is not None:
-    #                 image = self.load_images(option.cget("text"))
-
-    #             new_dropdown.add_option(
-    #                 option=option.cget("text"),
-    #                 image = image,
-    #                 command=option._command
-    #             )
-
-    #     return new_dropdown
-
-    # def configure_light_mode(self):
-    #     customtkinter.set_appearance_mode("light")
-
-    #     self.toolbar.configure(bg_color=self.MENUBAR_BACKGROUND_COLOR_LIGHT)
-    #     # self.file_button.configure(text_color=self.TEXT_COLOR_LIGHT, hover_color=self.HOVER_COLOR_LIGHT)
-    #     # self.settings_button.configure(text_color=self.TEXT_COLOR_LIGHT, hover_color=self.HOVER_COLOR_LIGHT)
-    #     # self.about_button.configure(text_color=self.TEXT_COLOR_LIGHT, hover_color=self.HOVER_COLOR_LIGHT)
-
-    #     self.file_button_dropdown = self.updated_dropdown(self.file_button, self.file_button_dropdown, self.DROPDOWN_BACKGROUND_COLOR_LIGHT, self.TEXT_COLOR_LIGHT, self.HOVER_COLOR_LIGHT)
-    #     self.settings_button_dropdown = self.updated_dropdown(self.settings_button, self.settings_button_dropdown, self.DROPDOWN_BACKGROUND_COLOR_LIGHT, self.TEXT_COLOR_LIGHT, self.HOVER_COLOR_LIGHT)
-    #     self.about_button_dropdown = self.updated_dropdown(self.about_button, self.about_button_dropdown, self.DROPDOWN_BACKGROUND_COLOR_LIGHT, self.TEXT_COLOR_LIGHT, self.HOVER_COLOR_LIGHT)
-
-    # def load_light_mode(self):
-    #     self.configure_light_mode()
-        
-    #     self.config.set('Settings', 'theme', 'light')
-
-    #     with open(self.config_file, 'w') as configfile:
-    #         self.config.write(configfile)
-
-    # def configure_dark_mode(self):        
-    #     customtkinter.set_appearance_mode("dark")
-
-    #     self.toolbar.configure(bg_color=self.MENUBAR_BACKGROUND_COLOR_DARK)
-    #     # self.file_button.configure(text_color=self.TEXT_COLOR_DARK, hover_color=self.HOVER_COLOR_DARK)
-    #     self.settings_button.configure(text_color=self.TEXT_COLOR_DARK, hover_color=self.HOVER_COLOR_DARK)
-    #     self.about_button.configure(text_color=self.TEXT_COLOR_DARK, hover_color=self.HOVER_COLOR_DARK)
-
-    #     self.file_button_dropdown = self.updated_dropdown(self.file_button, self.file_button_dropdown, self.DROPDOWN_BACKGROUND_COLOR_DARK, self.TEXT_COLOR_DARK, self.HOVER_COLOR_DARK)
-    #     self.settings_button_dropdown = self.updated_dropdown(self.settings_button, self.settings_button_dropdown, self.DROPDOWN_BACKGROUND_COLOR_DARK, self.TEXT_COLOR_DARK, self.HOVER_COLOR_DARK)
-    #     self.about_button_dropdown = self.updated_dropdown(self.about_button, self.about_button_dropdown, self.DROPDOWN_BACKGROUND_COLOR_DARK, self.TEXT_COLOR_DARK, self.HOVER_COLOR_DARK)
-
-    # def load_dark_mode(self):
-    #     self.configure_dark_mode()
-
-    #     self.config.set('Settings', 'theme', 'dark')
-
-    #     with open(self.config_file, 'w') as configfile:
-    #         self.config.write(configfile)
-    
-    # def load_system_mode(self):
-    #     customtkinter.set_appearance_mode("system")
-    #     appearance_mode = customtkinter.get_appearance_mode()
-    
-    #     if appearance_mode == "light":
-    #         self.configure_light_mode()
-    #     else:
-    #         self.configure_dark_mode()
-    
-    #     self.config.set('Settings', 'theme', 'system')
-    
-    #     with open(self.config_file, 'w') as configfile:
-    #         self.config.write(configfile)
+        with open(self.config_file, 'w') as configfile:
+            self.config.write(configfile)
 
     def open_browser(self):
         webbrowser.open_new(self.SOURCE_CODE_URL)
 
     def on_enter(self, event, tooltip):
         tooltip.get()
-
-    def on_leave(self, event, text):
-        print(text)
 
 def main():
     app = CrackExcel()
